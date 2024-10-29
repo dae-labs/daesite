c5ed353c5cc60fd1fe053f3bdfde64e0dac5005e
Dmitriy Chukhno <me@dmitr1sdae.com>
2024-10-29 19:33:04 +0100

    feat(gateway): remove unused code

--- services/gateway/src/main.rs
+++ services/gateway/src/main.rs
@@ -1,5 +1,3 @@
-use std::net::SocketAddr;
-
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
@@ -7,6 +5,6 @@ static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 async fn main() {
     env_logger::init();
 
-    let addr: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+    let addr = "127.0.0.1:8080".parse().unwrap();
     gateway::run_server(addr).await;
 }
